33941139385e0edc34ed88c022002cf19a5035aa
karlamarice <karlamarice@yahoo.com>
2017-11-30 01:56:27 +0800

    Add files via upload

---
+++ home/employee/Emp-asset.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+?>
+
+<html>
+    <head>
+        <title>Procure to Pay</title>
+        <link rel="stylesheet" type="text/css" href ="template.css">
+        <script language="javascript" src="Tabs.js"></script>
+    </head>
+
+    <body>
+        <!--Top Navigation bar-->
+        <!--get DB image, name, position-->
+        <header>
+            <div id="profile">    
+                <img class="profile-pic" src="../../res/N.png"/>
+                <h1> <?php echo("{$_SESSION['name']}"); ?> </h1> <br>
+                <div id="vl"><p> <?php echo("{$_SESSION['position']}"); ?> </p></div> 
+            </div>
+        </header>
+
+        <!--Side Navigation bar-->
+        <nav>
+			<ul class="ul-sidenav">
+                <li class="li-sidenav"><a href="Employee.php"><img class="icons" src="../../res/H.png"/> Home </a></li> 
+                <li class="li-sidenav"><a href="#"><img class="icons" src="../../res/ass.png"/> Asset Management </a></li>
+                <li class="li-sidenav"><a href="Emp-acct.php"><img class="icons" src="../../res/MA.png"/>My Account</a></li>
+                <li class="li-sidenav"><a href="Emp-out.php"><img class="icons" src="../../res/SO.png"/>Sign out</a></li>
+			</ul>
+        </nav>
+
+    </body>
+</html>
